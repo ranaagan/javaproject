dd2ef8282a1c06ef5543baf4208f90324b3056e4
raagan <raagan@USATLRAAGAN01.us.deloitte.com>
2024-01-31 10:28:09 -0500

    updating local path of DB

--- testing/src/Utilities/ConnectionFactory.java
+++ testing/src/Utilities/ConnectionFactory.java
@@ -6,9 +6,10 @@ import java.sql.DriverManager;
 
 public class ConnectionFactory {
 	  public static Connection getConnection(){
+		  String systemUser = System.getProperty("user.name");
 	      try {
 	    	  Class.forName("net.ucanaccess.jdbc.UcanaccessDriver");
-	          Connection connection = DriverManager.getConnection("jdbc:ucanaccess://C:\\Users\\ragan\\Desktop\\Java Projects\\Database1.accdb");
+	          Connection connection = DriverManager.getConnection("jdbc:ucanaccess://C:\\Users\\"+systemUser+"\\Desktop\\Java Project\\Database1.accdb");
 	          return connection;
 	      } catch (Exception ex) {
 	          throw new RuntimeException("Error connecting to the database", ex);
