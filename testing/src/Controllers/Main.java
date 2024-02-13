59961172fe1cc4e4403233ac18ddbd22d0b84265
Rana <ranaagan@gmail.com>
2024-02-13 18:19:56 -0500

    update

--- testing/src/Controllers/Main.java
+++ testing/src/Controllers/Main.java
@@ -6,7 +6,6 @@ public class Main {
 
 	public static void main(String[] args) {
 		SwingWindow window = new SwingWindow();
-
 		SwingWindowViewController controller = new SwingWindowViewController(window);
 		
 	}
